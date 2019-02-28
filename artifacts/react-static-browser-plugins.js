5789f1feb64a758dc9adbc04eecce77b9be97702
bf <bf@doordash.com>
2019-02-28 02:57:55 -0800

    gh-pages

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,17 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/bf/projects/bfleischhacker.github.io/node_modules/react-static-plugin-typescript",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/bf/projects/bfleischhacker.github.io",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
